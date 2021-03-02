6e617d5a22db1206d17b08a9c5a987bfb104620d
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-03-02 15:02:29 -0600

    save

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
+import app from './main/app';
 import startServer from './main/server';
 import connectToDb from './databases';
-import app from './main/app';
 
 dotenv.config({ path: '.env' });
 
