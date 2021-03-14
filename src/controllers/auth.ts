b53f2bc537489dd77d2ec692eb0e6cdc03630ad4
ahmednkhan24 <ahmednkhan24@gmail.com>
2021-03-13 18:53:05 -0600

    extra error handling

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,16 +2,22 @@ import { Request, Response } from 'express';
 import sanitizer from 'sanitizer';
 import isEmpty from 'lodash/isEmpty';
 import { registerNewUser } from '../services/auth';
-import { hasAllKeys } from '../utils';
+import { hasAllKeys, createApiResponse } from '../utils';
 
 export const signUp = async (req: Request, res: Response) => {
   if (isEmpty(req.body) || !hasAllKeys(req.body, ['email', 'password'])) {
-    return res.send({
-      error: 'must provide email and password in body of request',
-    });
+    const responseObj = createApiResponse(400, true);
+    return res.status(responseObj.statusCode).send(responseObj);
   }
+
   const email = sanitizer.sanitize(req.body.email);
   const password = sanitizer.sanitize(req.body.password);
+
+  if (!email || !password) {
+    const responseObj = createApiResponse(400, true);
+    return res.status(responseObj.statusCode).send(responseObj);
+  }
+
   const responseObj = await registerNewUser({ email, password });
   return res.status(responseObj.statusCode).send(responseObj);
 };
